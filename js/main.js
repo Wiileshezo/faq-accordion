430ecebf29003cb858acab47cbb539e2584d90d6
Wiileshezo <wiileshezo@gmail.com>
2024-01-16 20:38:12 +0200

    adding media queries fix the accordion close and open

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+var accordion = document.getElementsByClassName("accordion-title");
+accordion[0].nextElementSibling.style.display = "block";
+document.querySelectorAll(".plus")[0].src = "assets/images/icon-minus.svg";
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
+for (let i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function () {
+    for (let j = 0; j < accordion.length; j++) {
+      const close = accordion[j];
+      if (i == j) continue;
+      document.querySelectorAll(".plus")[j].src = "assets/images/icon-plus.svg";
+      close.classList.remove("active");
+      close.nextElementSibling.style.display = "none";
+    }
     this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+    var accordionContent = this.nextElementSibling;
+    if (accordionContent.style.display === "block") {
+      accordionContent.style.display = "none";
+      document.querySelectorAll(".plus")[i].src = "assets/images/icon-plus.svg";
     } else {
-      panel.style.display = "block";
+      accordionContent.style.display = "block";
+      document.querySelectorAll(".plus")[i].src =
+        "assets/images/icon-minus.svg";
     }
   });
 }
